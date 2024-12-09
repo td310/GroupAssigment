2413f518377e8aee9c10e60a4ab1c180474f969d
Tai <ndtai0912@gmail.com>
2024-12-09 16:03:56 +0700

    update

--- static/filter_test.py
+++ static/filter_test.py
@@ -72,15 +72,12 @@ def test_case_01(driver):
 
 #TC_ID: C_02 - Kiểm tra lọc ngày hợp lệ (Khoảng thời gian không có dữ liệu)
 def test_case_02(driver):
-    #tìm ngày bắt đầu và ngày kết thúc
     date_from_input = driver.find_element(By.NAME, "date-from")
     date_to_input = driver.find_element(By.NAME, "date-to")
 
-    #nhập dữ liệu ngày bắt đầu và kết thúc
     date_from_input.send_keys("01-01-2025") 
     date_to_input.send_keys("12-31-2025'")  
 
-    #bấm nút tìm kiếm
     submit_button = driver.find_element(By.NAME, "submit")
     submit_button.click()
     time.sleep(2)
@@ -93,53 +90,42 @@ def test_case_02(driver):
 
 #TC_ID: C_03 - Kiểm tra ngày bắt đầu lớn hơn ngày kết thúc.
 def test_case_03(driver):
-    #tìm ngày bắt đầu và ngày kết thúc
     date_from_input = driver.find_element(By.NAME, "date-from")
     date_to_input = driver.find_element(By.NAME, "date-to")
 
-    #nhập dữ liệu ngày bắt đầu và kết thúc
     date_from_input.send_keys("12-31-2024")
     date_to_input.send_keys("01-01-2023")
     
-    #bấm nút tìm kiếm
     submit_button = driver.find_element(By.NAME, "submit")
     submit_button.click()
     time.sleep(2)
     
-    #lấy nội dung từ alert
     alert = driver.switch_to.alert
     alert_text = alert.text
 
-    #kiểm tra alert
     expected_alert_text = "Ngày bắt đầu phải nhỏ hơn hoặc bằng ngày kết thúc!"
     assert alert_text == expected_alert_text
 
-    #bấm vào ok trong alert
     alert.accept()
 
 #TC_ID: C_04 - Kiểm tra không điền ngày.
 def test_case_04(driver):
-    #bấm nút tìm kiếm
     submit_button = driver.find_element(By.NAME, "submit")
     submit_button.click()
     
-    #TH1: Không có đơn hàng vào thời gian hiện tại
     no_result_message = "Không có kết quả thống kê nào trong khoảng thời gian này để hiển thị!"
     try:
         message_element = driver.find_element(By.XPATH, "//td[@colspan='6']")
         assert message_element.text == no_result_message, \
             f"Thông báo mong đợi: '{no_result_message}', nhưng nhận được '{message_element.text}'"
     except:
-        #TH2: Có đơn hàng vào thời gian hiện tại
         rows = driver.find_elements(By.XPATH, "//table[@class='statistic-product-sale__table']/tbody/tr")
         assert len(rows) > 0
 
 #TC_ID: C_05 - Kiểm tra điền ngày bắt đầu nhưng không điền ngày kết thúc.
 def test_case_05(driver):
-    #nhập ngày bắt đầu
     driver.find_element(By.NAME, "date-from").send_keys("12-31-2024")
 
-    #bấm nút tìm kiếm
     submit_button = driver.find_element(By.NAME, "submit")
     submit_button.click()
     time.sleep(2)
@@ -149,14 +135,9 @@ def test_case_05(driver):
  
 # TC_ID: C_06 - Kiểm tra điền ngày kết thúc nhưng không điền ngày bắt đầu.
 def test_case_06(driver):
-
-    #tìm ngày bắt đầu và ngày kết thúc
     date_to_input = driver.find_element(By.NAME, "date-to")
-
-    #nhập dữ liệu ngày bắt đầu và kết thúc
     date_to_input.send_keys("12-31-2024")
 
-    #bấm nút tìm kiếm
     submit_button = driver.find_element(By.NAME, "submit")
     submit_button.click()
     
@@ -173,21 +154,17 @@ def test_case_06(driver):
 
 #TC_ID: C_07 - Kiểm tra lọc ngày với nhiều trang kết quả.
 def test_case_07(driver):
-    # Tìm và nhập ngày bắt đầu và ngày kết thúc
     date_from_input = driver.find_element(By.NAME, "date-from")
     date_to_input = driver.find_element(By.NAME, "date-to")
     date_from_input.send_keys("01-01-2023")
     date_to_input.send_keys("12-31-2024")
 
-    # Bấm nút tìm kiếm
     submit_button = driver.find_element(By.NAME, "submit")
     submit_button.click()
     time.sleep(2)
 
-    # Kiểm tra sự tồn tại của phân trang
     paging_div = driver.find_element(By.CLASS_NAME, "paging")
     paging_items = paging_div.find_elements(By.CLASS_NAME, "paging__item")
 
-    # Kiểm tra sự tồn tại của trang 2
     page_2 = next((item for item in paging_items if item.text == "2"), None)
     assert page_2 is not None
